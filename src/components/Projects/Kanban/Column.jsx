26310ae9f952b6ec4ba868dc0db9619688a8b622
Kaung Htet Myat <programmingrelated1999@gmail.com>
2022-10-24 20:38:41 -0400

    buttons added

--- src/components/Projects/Kanban/Column.jsx
+++ src/components/Projects/Kanban/Column.jsx
@@ -1,24 +1,48 @@
 import React from 'react'
-import { Container, Card } from 'react-bootstrap'
+import { Container, Card, Button } from 'react-bootstrap'
+import { Chip } from '@mui/material'
 
 import "./Column.css"
 
-const Column = ({name, tasks}) => {
+const Column = ({name, tasks, bugs}) => {
+  console.log("Tasks", tasks);
+  console.log("Bugs", bugs);
 
-  return (
-    <Container className = "text-center my-4 mx-5 kanban-category">
+  return (        
+    <Container className = "my-4 mx-5 kanban-category">
         <h3 className = "kanban-title">{name}</h3>
-      <Container className= {`kanban-category-body py-3 background-${name}`}>
-        {tasks.map(task => 
-        <Card className = "my-1 kanban-card" style = {{width: "18rem"}} key = {task.name}>
-          <Card.Title>
-            {task.name}
-          </Card.Title>
-          <Card.Body>
-            {task.id}
-          </Card.Body>
-        </Card>)}
-      </Container>
+          <Container className= {`kanban-category-body py-3 background-${name}`}>
+            {tasks.map(task => (
+                <Card style={{ width: '18.5rem' }} className = "kanban-card my-2">
+                  <Card.Title>{task.name}</Card.Title>
+                  <Card.Body>
+                      <Chip label = {`Created on ${task.createdDate}`} color="primary" size="small" />
+                      <Card.Text>
+                        {task.description}
+                      </Card.Text>
+                  </Card.Body>
+                  <Card.Footer className = "d-flex justify-content-end">
+                    <Button className = "mx-1"> &lt; </Button>
+                    <Button className = "mx-1"> &gt; </Button>
+                  </Card.Footer>
+                </Card>
+            ))}
+            {bugs.map(bug => (
+              <Card style={{ width: '18.5rem' }} className = "kanban-card my-2">
+                <Card.Title>{bug.name}</Card.Title>
+                <Card.Body>
+                  <Chip label = {`Created on ${bug.createdDate}`} color="primary" size="small" />
+                  <Card.Text>
+                    {bug.description}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer className = "d-flex justify-content-end">
+                    <Button className = "mx-1"> &lt; </Button>
+                    <Button className = "mx-1"> &gt; </Button>
+                </Card.Footer>
+              </Card>
+            ))}
+          </Container>
     </Container>
   )
 }
--- src/components/Projects/tabs/projectKanban.jsx
+++ src/components/Projects/tabs/projectKanban.jsx
@@ -5,10 +5,13 @@ import Column from '../Kanban/Column';
 
 const projectKanban = () => {
 
-  const allTasks = useSelector(state => state.currentProject.projectData.tasks);
-  const allBugs = useSelector(state => state.currentProject.projectData.bugs);
+  let allTasks = useSelector(state => state.currentProject.projectData.tasks);
+  let allBugs = useSelector(state => state.currentProject.projectData.bugs);
   const isLoading = useSelector(state=>state.currentProject.isLoading);
   const hasError = useSelector(state=>state.currentProject.hasError);
+  
+  const[taskSelected, setTaskSelected] = useState();
+  const[bugSelected, setBugSelected] = useState();
 
   if(isLoading){
     return <p>Is Loading</p>
@@ -18,34 +21,36 @@ const projectKanban = () => {
     return <p>Has Error</p>
   }
 
-  const columns = [
-    {name: 'Started', taskIds: [1,2]},
-    {name: 'Progress', taskIds: [3,4]},
-    {name: 'Complete', taskIds: [5,6]}
-  ];
+  let tasksCreated = [];
+  let tasksProgress = [];
+  let tasksDone = [];
+  let bugsCreated = [];
+  let bugsProgress = [];
+  let bugsDone = [];
 
-  const tasks = [
-    {name: 'UX Development', id: 1},
-    {name: 'UI Development', id: 2},
-    {name: 'Web Development', id: 3},
-    {name: 'Andriod Development', id: 4},
-    {name: 'Phone Development', id: 5},
-    {name: 'Mobile Development', id: 6},
+  let tempTaskList = allTasks.filter(task => task.status === 'Created');
+  tasksCreated = tasksCreated.concat(tempTaskList);
+  tempTaskList = allTasks.filter(task => task.status === 'Progress');
+  tasksProgress = tasksProgress.concat(tempTaskList);
+  tempTaskList = allTasks.filter(task => task.status === 'Done');
+  tasksDone = tasksDone.concat(tempTaskList);
+  let tempBugList = allBugs.filter(bug => bug.status === 'Created');
+  bugsCreated = bugsCreated.concat(tempBugList);
+  tempBugList = allBugs.filter(bug => bug.status === 'Progress');
+  bugsProgress = bugsProgress.concat(tempBugList);
+  tempBugList = allBugs.filter(bug => bug.status === 'Done');
+  bugsDone = bugsDone.concat(tempBugList);
+
+  const columns = [
+    {name: "Started", taskList: tasksCreated, bugList: bugsCreated},
+    {name: "Progress", taskList: tasksProgress, bugList: bugsProgress},
+    {name: "Done", taskList: tasksDone, bugList: bugsDone},
   ]
 
   return (
     <div className="d-flex justify-content-around">
-      {columns.map((column) => {
-        const name = column.name;
-        let taskList = [];
-        for(let task of column.taskIds){
-          const foundTask = tasks.find(element => element.id === task);
-          taskList = taskList.concat(foundTask);
-        }
-        return (
-          <Column name = {name} tasks = {taskList} key = {name}></Column>
-        )
-      })}
+      {columns.map((column) => 
+          <Column name = {column.name} tasks = {column.taskList} bugs = {column.bugList} key={column.name}></Column>)}
     </div>
   )
 }
